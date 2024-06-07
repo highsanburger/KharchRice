c537328cf5352d030ae8c9beaf4bacce95d96e7e
Syed Khalid <magmastar167@gmail.com>
2024-06-07 20:01:02 +0530

    LO

--- post.sh
+++ post.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+#
+# TODO
+# fonts
+# LSPs whiptail
 
-sudo pacman --noconfirm -S neofetch git
+# sudo pacman --noconfirm -S neofetch git
+# read progs.csv and install everything
 
 git clone https://github.com/highsanburger/KharchRice
 
-cd KharchRice
+cd KharchRice || exit 1
 
 mv .config ..
 
